7f6a1e81239494e176cac5995ae95f27bbd8dc1e
yude <i@yude.jp>
2022-05-09 00:29:17 +0900

    Impl routing for curl

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
-const someHost = 'https://yude.pages.dev/';
-
-async function gatherResponse(response) {
-
-  const { headers } = response;
-  const contentType = headers.get('content-type') || '';
-  if (contentType.includes('application/json')) {
-    return JSON.stringify(await response.json());
-  } else if (contentType.includes('application/text')) {
-    return response.text();
-  } else if (contentType.includes('text/html')) {
-    return response.text();
-  } else {
-    return response.text();
-  }
-}
-
-async function handleRequest(request) {
-  // Return a new Response based on a URL's hostname
-  const url = new URL(request.url);
-
-  const init = {
-    headers: {
-      'content-type': 'text/html;charset=UTF-8',
-    },
-  };
-  const response = await fetch(url, init);
-  const results = await gatherResponse(response);
-
-  const userAgent = request.headers.get('User-Agent') || '';
-  if (userAgent.includes('bot')) {
-    return new Response('Block User Agent containing curl', { status: 403 });
-  } else {
-    return new Response(results, init);
-  }
-
-  console.error(
-    "Getting Client's IP address, device type, and ASN are not supported in playground. Must test on a live worker"
-  );
-  return fetch(request);
-}
-
-addEventListener('fetch', event => {
+addEventListener('fetch', function (event) {
   event.respondWith(handleRequest(event.request));
 });
+
+async function handleRequest(request) {
+  if (request.method !== 'GET') return MethodNotAllowed(request);
+  const url = new URL(request.url);
+  const userAgent = request.headers.get('User-Agent') || '';
+  const date = new Date();
+  const fmt_datetime = date.toLocaleString();
+  const curl_res = ""
+    + "yude.jp へようこそ!\n"
+    + "現在の日時は " + fmt_datetime + " です。\n"
+    + "\n"
+    + "Web: https://yude.jp\n"
+    + "GitHub: https://github.com/yudejp\n"
+    + "Twitter: https://twitter.com/yudejp\n"
+    + "電子メール: admin@yude.jp\n"
+    + "\n"
+    + "上記を表示している仕組みは、以下のリポジトリから閲覧できます。\n"
+    + "https://github.com/yudejp/yude.jp-routing\n"
+    + "\n";
+
+  if (userAgent.includes('curl')) {
+    return new Response(curl_res, { status: 200 });
+  } else {
+    return fetch(`https://yude.pages.dev` + url.pathname);
+  }
+}
+
+function MethodNotAllowed(request) {
+  return new Response(`Method ${request.method} not allowed.`, {
+    status: 405,
+    headers: {
+      Allow: 'GET',
+    },
+  });
+}
